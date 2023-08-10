1b1148e01ef9d40be711819a4da3c7ad7009b557
Andrew Grant <andrewgrant2009@hotmail.com>
2023-08-10 20:46:40 +0200

    added vim motions to take me to the moon

---
+++ app/hello.md
@@ -0,0 +1,18 @@
+
+
+
+
+
+BACK OF TH ELINE2.2.A2HELLO LOOK AT ME USING VIM MOTIONSI
+
+
+joe hello world!
+HlelOHEORLD!
+
+
+
+hjkl-- navigation
+left = h     right = l
+up = k     down = j
+
+jjRRahahahaha
--- app/notes/CreateNote.tsx
+++ app/notes/CreateNote.tsx
@@ -4,39 +4,37 @@ import { useRouter } from "next/navigation";
 import { SetStateAction, useState } from "react";
 
 export default function CreateNote() {
-  const [Title, setTitle] = useState('');
-  const [Content, setContent] = useState('');
-
+  const [Title, setTitle] = useState("");
+  const [Content, setContent] = useState("");
   const router = useRouter();
 
+  const create = async () => {
+    await fetch("http://127.0.0.1:8090/api/collections/notes/records", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Title,
+        Content,
+      }),
+    });
 
-  const create = async() => {
-
-await fetch("http://127.0.0.1:8090/api/collections/notes/records", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    Title,
-    Content,
-  }),
-});
-
- setContent('');
-    setTitle('');
+    setContent("");
+    setTitle("");
 
     router.refresh();
-}
+  };
 
-const titleChangehandler = (e: { target: { value: SetStateAction<string>; }; }) => {
-  // console.log(e);
-  // console.log(e.target.value);
-  setTitle(e.target.value)
-}
+  const titleChangehandler = (e: {
+    target: { value: SetStateAction<string> };
+  }) => {
+    // console.log(e);
+    // console.log(e.target.value);
+    setTitle(e.target.value);
+  };
 
   return (
-
     <form className="border-2 border-indigo-600" onSubmit={create}>
       <h3>Whats on your mind today?</h3>
       <input
@@ -45,14 +43,13 @@ const titleChangehandler = (e: { target: { value: SetStateAction<string>; }; }) 
         value={Title}
         onChange={titleChangehandler}
       />
-      <textarea className="border-2 border-indigo-600"
+      <textarea
+        className="border-2 border-indigo-600"
         placeholder="Share your toughts and musings..."
         value={Content}
         onChange={(e) => setContent(e.target.value)}
       />
-      <button type="submit">
-        Create Note
-      </button>
+      <button type="submit">Create Note</button>
     </form>
   );
 }
