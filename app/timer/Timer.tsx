6a503b5965cf88c96e96ad788b59d1f88975971f
Andrew Grant <andrewgrant2009@hotmail.com>
2023-08-19 21:55:06 +0200

    working on timer function need to add theme

--- app/timer/Timer.tsx
+++ app/timer/Timer.tsx
@@ -1,51 +1,48 @@
 "use client";
 import { useState, useEffect } from "react";
 
-function Timer() {
-  const [time, setTime] = useState(1500); // 25 minutes in seconds
-  const [isActive, setIsActive] = useState(false);
+const Timer: React.FC = () => {
+const [time, setTime] = useState(1500);
+const [timeLeft, setTimeLeft] = useState(time);
+const [running, setIsRunning] = useState(false);
 
-  useEffect(() => {
-    let interval: number | NodeJS.Timer | undefined;
+useEffect(() => {
+  if (running) {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTimeLeft) => prevTimeLeft -1);
+    }, 1000);
 
-    if (isActive && time > 0) {
-      interval = setInterval(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else if (time === 0) {
-      clearInterval(interval);
-    }
+return () => clearInterval(timer);
+  }
+}, [running]);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [isActive, time]);
+const handleTimeOption = (time: number) => {
+  setTime(time);
+  setTimeLeft(time);
+};
 
-  const toggleTimer = () => {
-    setIsActive(!isActive);
-  };
+return(
 
-  return (
-    <div className="bg-[#DA775D] text-[#FAF5ED] rounded m-8">
-      <div className="flex flex-col justify-center p-8">
-      <h1 className=" flex justify-center text-8xl">
-        {Math.floor(time / 60)}:{(time % 60).toString().padStart(2, "0")}
-      </h1>
+ <div className="bg-[#DA775D] text-[#FAF5ED] rounded m-8">
+       <div className="flex flex-col justify-center p-8">
+       <h1 className=" flex justify-center text-8xl">
+        {Math.floor(timeLeft / 60)}:{(timeLeft % 60).toString().padStart(2, "0")}
+       </h1>
 
-      <button
-        className="px-8 text-3xl flex justify-center"
-        onClick={toggleTimer}
-      >
-        {isActive ? "Pause" : "Start"}
-      </button>
-</div>
-      <div className="text-xl grid grid-cols-3 divide-x">
-        <button onClick={toggleTimer}> Focus</button>
-        <button onClick={toggleTimer}> Short Break</button>
-        <button onClick={toggleTimer}> Long Break</button>
-      </div>
-    </div>
-  );
+       <button
+         className="px-8 text-3xl flex justify-center"
+         onClick={() => setIsRunning(!running)}
+       >
+         {running ? "Pause" : "Start"}
+       </button>
+ </div>
+       <div className="text-xl grid grid-cols-3 divide-x">
+         <button onClick={() => handleTimeOption(1500)}> Focus</button>
+         <button onClick={() => handleTimeOption(300)}> Short Break</button>
+         <button onClick={() => handleTimeOption(900)}> Long Break</button>
+       </div>
+     </div>
+   );
 }
 
-export default Timer;
+ export default Timer;
