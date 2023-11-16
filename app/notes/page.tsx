e206430cca2d4b74222214a917c72eb9dc9732de
Andrew Grant <andrewgrant2009@hotmail.com>
2023-11-16 19:11:08 +0100

    use contains not search in findMany call 🐊

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -5,8 +5,11 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import SearchBar from "../SearchTest";
 
-
-export default async function NotesPage( { searchParams } : { searchParams: any  }) {
+export default async function NotesPage({
+  searchParams,
+}: {
+  searchParams: any;
+}) {
   const session = await getServerSession(authOptions);
   // console.log(session?.user?.name, "Hello there User!");
   const noteFeed = await prisma.note.findMany({
@@ -23,44 +26,48 @@ export default async function NotesPage( { searchParams } : { searchParams: any 
     },
   });
 
+  // .split(' ').join(" & "), ???
+  // let params = new URL(document.location).searchParams;
+  // let name = params.get("name"); // is the string "Jonathan Smith".
 
-// .split(' ').join(" & "), ???
-// let params = new URL(document.location).searchParams;
-// let name = params.get("name"); // is the string "Jonathan Smith".
+  // const param = searchParams;
+  // let newSearch = param.get("search");
 
-// const param = searchParams;
-// let newSearch = param.get("search");
+  // console.log(newSearch, "MAYBE")
 
-// console.log(newSearch, "MAYBE")
+  const urlParams = new URLSearchParams(searchParams);
+  const maybe = urlParams.get("search");
 
-const urlParams = new URLSearchParams(searchParams);
-const maybe = urlParams.get("search")
+  const query = maybe?.toString().split(" ").join(" <-> ");
 
-const query = maybe?.toString().split(' ').join(" & ");
+  //.replace(/[\s\n\t]/g, '_');
 
-//.replace(/[\s\n\t]/g, '_');
+  // const newQ = JSON.parse(query);
 
-// const newQ = JSON.parse(query);
+  const NoteSearch = await prisma.note.findMany({
+    where: {
+      //     // title: {
+      //     //   search: "test",
+      //     // },
+      content: {
+        contains: query,
+      },
+      //   //  color: {
+      //   //     search: "#FFD046",
+      //   //   },
+      //   },
 
-
-const NoteSearch = await prisma.note.findMany({
- where: {
-    // title: {
-    //   search: "test",
-    // },
-    content: {
-      search: query,
+      // orderBy: {
+      //   _relevance: {
+      //   fields: ['content'],
+      //       search: query,
+      //         }
     },
-  //  color: {
-  //     search: "#FFD046",
-  //   },
-  },
-})
+  });
 
-
-  console.log(typeof(searchParams), "have we a search??", searchParams)
-  console.log(typeof(NoteSearch), "a list?")
-console.log(typeof(query), "here is a query?", query)
+  console.log(typeof searchParams, "have we a search??", searchParams);
+  console.log(typeof NoteSearch, "a list?");
+  console.log(typeof query, "here is a query?", query);
 
   type Props = {
     noteFeed: NoteProps[];
@@ -69,29 +76,24 @@ console.log(typeof(query), "here is a query?", query)
   const currentUser = session?.user?.name;
   // const firstName = currentUser?.split(" ")
 
-
   return (
     <>
- <SearchBar />
+      <SearchBar />
 
       <div className="row-span-3 grid gap-2 grid-cols-3 m-4">
         <CreateNote />
         {noteFeed?.map((noteFeed) => {
           return <Note key={noteFeed.id} note={noteFeed} />;
         })}
-
       </div>
-
-
 
       <p>QUERY:{query}</p>
 
       {/* <p>SEARCHPARAMS:{searchParams}</p> */}
-
 
       {NoteSearch?.map((noteFeed) => {
         return <Note key={noteFeed.id} note={noteFeed} />;
       })}
     </>
   );
-};
+}
