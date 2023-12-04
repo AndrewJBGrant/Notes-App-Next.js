6aaca6103eaa6472fdd5d9b63307c812c6f5efa9
Andrew Grant <andrewgrant2009@hotmail.com>
2023-12-04 19:19:09 +0100

    rendering most recent journal post and adding better auth options

--- app/AuthChecker.tsx
+++ app/AuthChecker.tsx
@@ -27,6 +27,17 @@ export default function AuthChecker({
   } else {
     return (
       <>
+
+Welcome to focus Notes a quiet corner of the net
+Use a quick note to something down quick.
+
+A journal enrty to keep track of your daily progress/ideas etc...
+
+A pomodoro timer is a great way to stay focused while making sure to give yourself enough breaks
+
+And a todo list to stay on top of daily tasks
+
+
         <SignInButton />
       </>
     );
